b071a9587445f306b799a324330a7ad9f088e411
cemim <filipe.cemim@gmail.com>
2023-12-13 10:23:44 -0300

    Add Roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +18,18 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(StatusSeeder::class);
         $this->call(UserSeeder::class);
+        
+        Role::create(['name' => 'Administrador']);
+        Role::create(['name' => 'Usuario']);
+        Permission::create(['name' => 'editar tarefas']);
+        
+        // Atribuindo permissão à role "Administrador"
+        $role = Role::findByName('Administrador');
+        $role->givePermissionTo('editar tarefas');
+
+        // Atribuir Role ao usuário admin
+        $userAdmin = User::find(1); 
+        $roleAdministrador = Role::findByName('Administrador');
+        $userAdmin->assignRole($roleAdministrador);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,8 +17,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages.home');
-})->name('inicio');
+    return view('welcome');
+    //return view('pages.home');
+});//->name('inicio');
 
 Route::resource('tasks', TaskController::class);
 Route::resource('users', UserController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
